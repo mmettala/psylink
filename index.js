238b0d3e11226dee4f11a898287bf2952fe4b574
Feuer <feuer@mac>
2014-10-11 16:26:17 +0300

    A bit more hacking on the user-front, commit to keep git quiet on pull...

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // based on http://www.raywenderlich.com/61078/write-simple-node-jsmongodb-web-service-ios-app
 
 var http = require('http'),
-    express = require('express'),
-    path = require('path'),
-    MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    CollectionDriver = require('./collectionDriver').CollectionDriver;
-
+express = require('express'),
+path = require('path'),
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+CollectionDriver = require('./collectionDriver').CollectionDriver,
+u = require("./model/users");
+ 
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -24,61 +25,84 @@ var collectionDriver;
 
 var mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
 mongoClient.open(function(err, mongoClient) {
-  if (!mongoClient) {
-      console.error("Error! Exiting... Must start MongoDB first");
-      process.exit(1);
-  }
-  var db = mongoClient.db("MyDatabase");
-  collectionDriver = new CollectionDriver(db);
+    if (!mongoClient) {
+	console.error("Error! Exiting... Must start MongoDB first");
+	process.exit(1);
+    }
+    var db = mongoClient.db("MyDatabase");
+    collectionDriver = new CollectionDriver(db);
+    console.log("collectionDriver generated #1")
+    u.register_db(collectionDriver);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res){
-  res.render('frontpage');
+    res.render('frontpage');
 });
 
+app.get('/register', function(req, res){
+    res.render('questionnaire');
+}); 
+
 app.get('/admin', function(req, res){
-  res.render('admin');
+    res.render('admin');
 });
 
 app.get('/data/:collection', function(req, res) {
-   var params = req.params;
-   collectionDriver.findAll(req.params.collection, function(error, objs) {
+    var params = req.params;
+    collectionDriver.findAll(req.params.collection, function(error, objs) {
         if (error) { res.send(400, error); }
         else {
             if (req.accepts('html')) {
                 res.render('data',{objects: objs, collection: req.params.collection});
-              } else {
-            res.set('Content-Type','application/json');
-                  res.send(200, objs);
-              }
-         }
+            } else {
+		res.set('Content-Type','application/json');
+                res.send(200, objs);
+            }
+        }
     });
 });
 
 app.get('/data/:collection/:entity', function(req, res) {
-   var params = req.params;
-   var entity = params.entity;
-   var collection = params.collection;
-   if (entity) {
-       collectionDriver.get(collection, entity, function(error, objs) {
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); }
-       });
-   } else {
-      res.send(400, {error: 'bad url', url: req.url});
-   }
+    var params = req.params;
+    var entity = params.entity;
+    var collection = params.collection;
+    if (entity) {
+	collectionDriver.get(collection, entity, function(error, objs) {
+            if (error) { res.send(400, error); }
+            else { res.send(200, objs); }
+	});
+    } else {
+	res.send(400, {error: 'bad url', url: req.url});
+    }
 });
 
 app.post('/data/:collection', function(req, res) {
     var object = req.body;
     var collection = req.params.collection;
     collectionDriver.save(collection, object, function(err,docs) {
-          if (err) { res.send(400, err); }
-          else { res.send(201, docs); }
-     });
+        if (err) { res.send(400, err); }
+        else { res.send(201, docs); }
+    });
+});
+
+app.post('/do_insert', function(req, res){ //Server returns JSON from somewhere if the path is /data/insert
+    var form = req.body;//,
+    user = u.create_user(form.name,
+			 form.age,
+			 form.male == "on"? "male":"female",
+			 [form.answer, true, form.doctorSex],
+			 form.pwd);
+
+    u.save_user(user, function(success)
+		{
+		    if(success) res.send(200, "Success!")
+		    else res.send(400, "Failure!");
+		});
+    
+    //res.send(200, "Hello "+req.body.name+"! Your text is "+req.body.answer);
 });
 
 app.put('/data/:collection/:entity', function(req, res) {
@@ -86,14 +110,14 @@ app.put('/data/:collection/:entity', function(req, res) {
     var entity = params.entity;
     var collection = params.collection;
     if (entity) {
-       collectionDriver.update(collection, req.body, entity, function(error, objs) {
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); }
-       });
-   } else {
-       var error = { "message" : "Cannot PUT a whole collection" };
-       res.send(400, error);
-   }
+	collectionDriver.update(collection, req.body, entity, function(error, objs) {
+            if (error) { res.send(400, error); }
+            else { res.send(200, objs); }
+	});
+    } else {
+	var error = { "message" : "Cannot PUT a whole collection" };
+	res.send(400, error);
+    }
 });
 
 app.delete('/data/:collection/:entity', function(req, res) {
@@ -101,14 +125,14 @@ app.delete('/data/:collection/:entity', function(req, res) {
     var entity = params.entity;
     var collection = params.collection;
     if (entity) {
-       collectionDriver.delete(collection, entity, function(error, objs) {
-          if (error) { res.send(400, error); }
-          else { res.send(200, objs); }
-       });
-   } else {
-       var error = { "message" : "Cannot DELETE a whole collection" };
-       res.send(400, error);
-   }
+	collectionDriver.delete(collection, entity, function(error, objs) {
+            if (error) { res.send(400, error); }
+            else { res.send(200, objs); }
+	});
+    } else {
+	var error = { "message" : "Cannot DELETE a whole collection" };
+	res.send(400, error);
+    }
 });
 
 
@@ -117,5 +141,5 @@ app.use(function (req,res) {
 });
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- model/users.js
+++ model/users.js
@@ -1,25 +1,31 @@
-var users = function()
+db = null;
+
+exports.register_db = function (connectiondriver)
 {
-    var db = null;
+    db = connectiondriver;
 };
 
-exports.register_db = function (_db)
-{
-    db = _db;
-};
-
-exports.create_user = function(name, age, sex, answers)
+exports.create_user = function(name, age, sex, answers, pwd)
 {
     return {name: name,
 	    age: age,
 	    sex: sex,
+	    hash: pwd,
 	    answers: answers}; //One can't get the list of argument-names and zipwith arguments' values in JS?
 };
 
 exports.find_user = function(search_terms)
 {
     if(db == null) console.log( "Db not registered");
-    else  return db.users.find(search_terms);
+//    else db.
 };
 
-exports.users = users;
+exports.save_user = function(user_obj, callback)
+{
+    db.save("Users", user_obj, function(error, stuff)
+	    {
+		if(error) callback(false);
+		else callback(true);
+	    });
+}
+
